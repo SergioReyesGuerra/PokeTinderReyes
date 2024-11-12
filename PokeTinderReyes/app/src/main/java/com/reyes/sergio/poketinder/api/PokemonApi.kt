b5e3d6c0051c85340c9dcd1781114897a7814031
Tu Nombre <sergio.reyes@tecsup.edu.pe>
2024-11-11 23:24:20 -0500

    Initial commit

---
+++ PokeTinderReyes/app/src/main/java/com/reyes/sergio/poketinder/api/PokemonApi.kt
@@ -0,0 +1,10 @@
+package com.reyes.sergio.poketinder.api
+
+import com.reyes.sergio.poketinder.PokemonListResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface PokemonApi {
+    @GET("/api/v2/pokemon")
+    suspend fun getPokemons(): Response<PokemonListResponse>
+}
